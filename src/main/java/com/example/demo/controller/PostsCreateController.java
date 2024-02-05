9c80b0efc104b90949f09f674f74c9d092ba78ea
Reholly <eu.ustiantsev@yandex.ru>
2024-02-05 11:38:20 +0500

    done 7

---
+++ src/main/java/com/example/demo/controller/PostsCreateController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Post;
+import com.example.demo.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PostsCreateController {
+
+    private final PostService postsService;
+
+    public PostsCreateController(PostService postsService) {
+        this.postsService = postsService;
+    }
+    @RequestMapping(path = "/new", method = RequestMethod.POST  )
+    public String doCreate(@ModelAttribute("text") String text) {
+
+        postsService.create(text);
+        return "redirect:/";
+    }
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public String newPost(Model model) {
+
+        return "create";
+    }
+}
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -6,18 +6,23 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 
 @Service
 public class PostService {
 
-    public ArrayList<Post> listAllPosts() {
-        ArrayList<Post> posts = new ArrayList<>();
-
+    private ArrayList<Post> posts;
+    public PostService() {
+        posts = new ArrayList<>();
         posts.add(new Post("Трудящийся медведь", new Date(2023, Calendar.JANUARY, 25)));
         posts.add(new Post("Трудящийся человек",  new Date(2021, Calendar.OCTOBER, 1)));
         posts.add(new Post("Трудящийся зайчик",  new Date(2009, Calendar.NOVEMBER, 5)));
 
+    }
+    public ArrayList<Post> listAllPosts() {
         return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
